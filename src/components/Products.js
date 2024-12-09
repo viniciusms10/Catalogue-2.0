56d0b98c7cae50bcf3c828d172d6f8cdea2ffbbf
Vinicius <viniciusms2012@hotmail.com>
2024-12-09 18:48:43 -0300

    Adicionadas as imagens,preços e finalização da transição para React

---
+++ src/components/Products.js
@@ -0,0 +1,14 @@
+function Products({src,name,price}) {
+    return(
+        <div class="show-item">
+            <img src={src} alt=""></img>
+            <label className="product-name" for="img">{name}</label>
+            <br></br>
+            <br></br>
+            <label className="product-price" for="img">R$ {price}</label>
+            <p>Ver mais</p>
+        </div>
+    );
+}
+
+export default Products
---
+++ src/components/Rooms.js
@@ -0,0 +1,15 @@
+function Rooms({name}) {
+    return (
+        <div className={name}>
+            <div className="front">
+                <p>{name}</p>
+            </div>
+
+            <div className="back">
+
+            </div>
+        </div>
+    )
+}
+
+export default Rooms
